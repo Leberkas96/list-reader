5b722b5e643ea8e0633f1ba6325e43b36edc48f4
Konrad Kaminski <konrad.kaminski50@gmail.com>
2023-04-05 22:56:06 +0200

    reformatting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
-module at.htlhl.bschach {
+module list.listreader {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires java.desktop;
-
-    opens list to javafx.fxml;
-    exports list;
-    //Removed module in compiler settings: --add-exports java.datatransfer/java.awt=at.htlhl.bschach,at.htlhl.bschach
+            
+        requires org.controlsfx.controls;
+                            
+    opens list.listreader to javafx.fxml;
+    exports list.listreader;
 }
